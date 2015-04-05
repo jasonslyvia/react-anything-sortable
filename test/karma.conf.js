1a97248eef79ea3ce266d51a66f551552d2a3a4c
jasonslyvia <jasonslyvia@gmail.com>
2015-04-05 22:43:37 +0800

    adjust test conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -73,16 +73,16 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Firefox', 'Chrome'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
